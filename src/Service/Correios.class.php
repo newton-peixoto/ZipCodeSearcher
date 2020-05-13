ee2018f7dc834d8384cb00ccb357ff656f9955cd
Breno Campos <breno23augusto@hotmail.com>
2020-05-13 16:56:42 -0300

    :bug:

--- src/Service/Correios.class.php
+++ src/Service/Correios.class.php
@@ -2,6 +2,8 @@
 
 namespace ZipCode\Service;
 
+use XMLReader;
+
 class Correios
 {
     private $xml;
@@ -12,33 +14,45 @@ class Correios
     {
         $this->setXml($cep);
         $this->url = 'https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente';
-        $this->header = array('Content-Type: text/plain', "cache-control:no-cache");
+        $this->header = array('text/xml;charset=UTF-8', 'cache-control:no-cache');
     }
-    
+
     public function fetchData()
     {
         $ch = curl_init();
-
-        curl_setopt($ch,CURLOPT_URL, $this->url);
-        curl_setopt($ch,CURLOPT_POST, true);
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch,CURLOPT_POSTFIELDS, $this->xml);
-        curl_setopt($ch, CURLOPT_HTTPHEADER,     $this->header);
+        curl_setopt($ch, CURLOPT_URL, $this->url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6");
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $this->xml);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->header);
 
         $result = curl_exec($ch);
+        echo curl_error($ch);
         curl_close($ch);
-
-        return $result;
+        return utf8_encode($result);
     }
 
 
     private function setXml($cep)
     {
-        $this->xml ='<?xml version="1.0"?><soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:cli="http://cliente.bean.master.sigep.bsb.correios.com.br/"><soapenv:Header/><soapenv:Body> <cli:consultaCEP><cep>75712480</cep></cli:consultaCEP> </soapenv:Body></soapenv:Envelope>';
-
+        $this->xml = <<<XML
+        <?xml version='1.0'?>
+        <soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' 
+          xmlns:cli='http://cliente.bean.master.sigep.bsb.correios.com.br/'>
+          <soapenv:Header/>
+          <soapenv:Body>
+            <cli:consultaCEP>
+              <cep>{$cep}</cep>
+            </cli:consultaCEP>
+          </soapenv:Body>
+        </soapenv:Envelope>
+        XML;
     }
 }
 
-$correio = new Correios('75712480');
+$correio = new Correios('75710808');
 
-echo $correio->fetchData();
+var_dump($correio->fetchData());
